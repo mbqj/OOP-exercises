52f8e21db2dae95a159b42d78121a3705333a0d8
VinciDa33 <marcusbthomsen@gmail.com>
2025-09-29 15:10:44 +0200

    Objects and classes solutions

---
+++ objects_and_classes/customer_database/Program.cs
@@ -0,0 +1,85 @@
+﻿class Program {
+	public static void Main(string[] args) {
+		CustomerDatabase database = new CustomerDatabase();
+
+		database.AddCustomer(new Customer("Peter", 0, 200));
+		database.AddCustomer(new Customer("Larry", 1));
+		database.AddCustomer(new Customer("Sarah", 2, 45));
+
+		database.PrintCustomers();
+	}
+}
+
+//Customer class
+class Customer {
+	public string name;
+	public int id;
+	public double balance;
+
+	//Constructor #1
+	public Customer(string name, int id) {
+		this.name = name;
+		this.id = id;
+		this.balance = 0;
+	}
+
+	//Constructor #2
+	public Customer(string name, int id, double balance) {
+		this.name = name;
+		this.id = id;
+		this.balance = balance;
+	}
+
+	//Deposit method
+	public void Deposit(double amount) {
+		balance += amount;
+	}
+
+	//Withdraw method
+	public void Withdraw(double amount) {
+		if (balance >= amount)
+			balance -= amount;
+	}
+
+	//Get balance method
+	public double GetBalance() {
+		return balance;
+	}
+}
+
+//Customer database class
+class CustomerDatabase {
+	Customer[] customers;
+
+	public CustomerDatabase() {
+		customers = new Customer[10];
+	}
+
+	public void AddCustomer(Customer customer) {
+		for (int i = 0; i < customers.Length; i++) {
+			if (customers[i] == null) {
+				customers[i] = customer;
+				break;
+			}
+		}
+	}
+
+	public void RemoveCustomerById(int id) {
+		for (int i = 0; i < customers.Length; i++)
+			if (customers[i].id == id)
+				customers[i] = null;
+	}
+
+	public Customer[] GetCustomers() {
+		return customers;
+	}
+
+	public void PrintCustomers() {
+		Console.WriteLine("<<< Printing customers >>>");
+		for (int i = 0; i < customers.Length; i++) {
+			if (customers[i] != null) {
+				Console.WriteLine("Name: " + customers[i].name + ", id: " + customers[i].id + ", balance: " + customers[i].balance);
+			}
+		}
+	}
+}
---
+++ objects_and_classes/customers/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program {
+	public static void Main(string[] args) {
+		Customer aCustomer; //Declaration
+		aCustomer = new Customer("Peter", 0, 500); //Instantiate a new customer (name=Peter, id=0, balance=500)
+		aCustomer.Deposit(100);
+		aCustomer.Withdraw(150);
+		Console.WriteLine(aCustomer.GetBalance());
+	}
+}
+
+class Customer {
+	string name;
+	int id;
+	double balance;
+
+	//Constructor #1
+	public Customer(string name, int id) {
+		this.name = name;
+		this.id = id;
+		this.balance = 0;
+	}
+
+	//Constructor #2
+	public Customer(string name, int id, double balance) {
+		this.name = name;
+		this.id = id;
+		this.balance = balance;
+	}
+
+	//Deposit method
+	public void Deposit(double amount) {
+		balance += amount;
+	}
+
+	//Withdraw method
+	public void Withdraw(double amount) {
+		if (balance >= amount)
+			balance -= amount;
+	}
+
+	//Get balance method
+	public double GetBalance() {
+		return balance;
+	}
+}
