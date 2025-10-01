fdec4dc25cad01b6681dda259ad0c1c0e55e4f06
VinciDa33 <marcusbthomsen@gmail.com>
2025-10-01 08:59:53 +0200

    Added solutions for exceptions

---
+++ exceptions/accounts/Program.cs
@@ -0,0 +1,35 @@
+﻿//T1 Marcus
+
+int[] accounts = {903, 716, 67};
+
+int GetAccountNumber()
+{
+    Console.WriteLine("Enter an account number: ");
+
+    //This Convert.ToInt32 may throw an exception, if the string cannot be converted to a number!
+    //We do not handle this exception here, so it will escape this function!
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintAccountState(int accountId)
+{
+    //Try catch used to handle the exception that could happen, if a number is outside the bounds of our array
+    try {
+        Console.WriteLine("Account "+ accountId+ " contains "+ accounts[accountId]);
+    }
+    catch (Exception e) {
+        Console.WriteLine("Account ID not recognized!\n");
+    }
+}
+
+while (true)
+{
+    //Try catch used to handle the exception that escapes the GetAccountNumber method!
+    try {
+        int accountId = GetAccountNumber();
+        PrintAccountState(accountId);
+    }
+    catch (Exception e) {
+        Console.WriteLine("Bad input!");
+    }
+}
---
+++ exceptions/average_grade/Program.cs
@@ -0,0 +1,29 @@
+﻿//T1 Marcus
+
+int[] grades = {4, 7, 2, 0, 10, 4, 12};
+
+int GetGrade(int courseId) {
+    int grade = grades[courseId];
+    if (grade < 2)
+        throw new Exception("Student failed!"); //Manually throwing an exception!
+    return grade;
+}
+
+int count = 0;
+int sum = 0;
+
+//We iterate through all grades
+for (int courseId = 0; courseId < grades.Length; courseId++) {
+    //GetGrade() may throw an exception, so we handle it with a try catch
+    try {
+        int grade = GetGrade(courseId);
+        //If an exception happended in GetGrade, it is caught immeadietly, and the 2 lines below this comment will not run!
+        count++;
+        sum += grade;
+    }
+    catch (Exception e) {
+        Console.WriteLine("Course " + courseId + ": " + e.Message);
+    }
+}
+
+Console.WriteLine("Average: " + (sum / count));
