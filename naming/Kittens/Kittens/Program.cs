025681d2a1fcee36859b1ed7c56dabff984a8a09
mbqj <mjurg22@student.sdu.dk>
2025-10-08 12:18:03 +0200

    Adds solution for Kittens and Timing

---
+++ naming/Kittens/Kittens/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Kitten
+{
+    private double cuteness;
+    public static int count = 0;
+
+    public Kitten(double cuteness)
+    {
+        this.cuteness = cuteness;
+        count++;
+    }
+    
+    
+}
+
+class Program
+{
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many cats?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Kitten[] kittens = new Kitten[n];
+            for (int i = 0; i < n; i++)
+            {
+                double cuteness = i;
+                kittens[i] = new Kitten(cuteness);
+            }
+            Console.WriteLine(Kitten.count);
+        }
+}
---
+++ naming/Timing/Timing/Program.cs
@@ -0,0 +1,23 @@
+﻿class Timing
+{
+    public static double Fun(double x, double y)
+    {
+        if (y<=1)
+        {
+            return x;
+        }
+
+        return Fun(x, y - 1) * Fun(x, y-1);
+    }
+
+    static void Main(string[] args)
+    {
+        for (int i = 1; i < 32; i++)
+        {
+            long latest = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            Console.WriteLine("Fun: " + Fun(1.0000001, i));
+            long now = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            Console.WriteLine("Time taken: " + (now-latest) + " Milliseconds");
+        }
+    }
+}
