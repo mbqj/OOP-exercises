bb8189ea277de4a4ee38c2e1c251ce5f3b57972b
mbqj <mjurg22@student.sdu.dk>
2025-11-05 11:41:13 +0100

    Adds testing solution

---
+++ Testing/Testing/Testing/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Pin
+{
+    const int CRITICAL_AMOUNT = 350;
+    const int MAX_TRANSACTIONS = 4;
+    private int time2pin = MAX_TRANSACTIONS;
+    public bool Expend (int amount) {
+        if (time2pin==0 || amount>CRITICAL_AMOUNT) {
+            time2pin = MAX_TRANSACTIONS;
+        }
+        return amount > CRITICAL_AMOUNT || --time2pin == 0;
+    }
+    public static void Main (string[] args) {
+        Pin pin = new Pin();
+        for (int i=0 ; i<20 ; i++) {
+            bool give_pin = pin.Expend(42);
+            Console.WriteLine(i + ": 42 -> " + give_pin);
+        }
+    }
+}
---
+++ Testing/Testing/Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework.Legacy;
+
+
+namespace Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Pin p = new Pin();
+        ClassicAssert.False(p.Expend(250));
+        ClassicAssert.True(p.Expend(351));
+        p.Expend(250);
+        p.Expend(250);
+        p.Expend(250);
+        ClassicAssert.True(p.Expend(250));
+        
+    }
+}
